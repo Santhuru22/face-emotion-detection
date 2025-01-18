e9a76c1a3f3d6eb0ae0eb3000a4c2912faed3e95
Santhuru22 <santak990@gmail.com>
2025-01-18 15:33:08 +0530

    emotion with ip.py

---
+++ emotion with ip.py
@@ -0,0 +1,23 @@
+from facial_emotion_recognition import EmotionRecognition
+import urllib.request
+import cv2
+import numpy as np
+import imutils
+
+er=EmotionRecognition(device='cpu')
+cam = cv2.videocapture(0)
+
+while True:
+    imgPath=urllib.request.urlopen(url)
+    imgNp=np.array(bytearray(imgPath.read()), dtype=np.uint8)
+    frame=cv2.imdecode(imgNp, -1)
+    
+    frame=er.recognise_emotion(frame,return_type='BGR')
+    frame=imutils.resize(frame,width=450)
+    cv2.imshow("Frame",frame)
+    key=cv2.waitKey(1)
+    if key==27:
+        break
+
+cv2.destroyAllWindows()
+    
